72447637533b9ac6a9d7fc59f434f692c6c52877
kurt <trainee.kurt.martin-brown@sigmalabs.co.uk>
2025-03-13 13:07:15 +0000

    can select multiple buttons, undo working

--- src/components/WordGuess.test.tsx
+++ src/components/WordGuess.test.tsx
@@ -24,4 +24,36 @@ describe("WordGuess", () => {
     // check the sentence text now says "I swim therefore _am"
     screen.getByText("I swim therefore _ am");
   });
+
+  test("can fill in two blank by clicking two options", () => {
+    render(<WordGuess />);
+
+    // query the screen for a button called "Swim"
+    let button = screen.getByRole("button", { name: "Swim" });
+    // click on the "Swim" button
+    fireEvent.click(button);
+
+    // query the screen for a button called "Think"
+    button = screen.getByRole("button", { name: "Think" });
+    // click on the "Think" button
+    fireEvent.click(button);
+
+    // check the sentence text now says "I swim therefore think am"
+    screen.getByText("I swim therefore think am");
+  });
+
+  test("can undo a guess by clicking undo", () => {
+    render(<WordGuess />);
+
+    // query the screen for a button called "Swim"
+    let button = screen.getByRole("button", { name: "Swim" });
+    // click on the "Swim" button
+    fireEvent.click(button);
+
+    button = screen.getByRole("button", { name: "Undo" });
+    fireEvent.click(button);
+
+    // check the sentence text now says "I _ therefore _ am"
+    screen.getByText("I _ therefore _ am");
+  });
 });
--- src/components/WordGuess.tsx
+++ src/components/WordGuess.tsx
@@ -4,7 +4,40 @@ import data from "../data.tsx";
 export default function WordGuess() {
   const [sentence, setSentence] = useState(data.sentence);
 
-  const displaySentence = data.sentence
+  function handleGuess(event: React.MouseEvent<HTMLButtonElement>) {
+    const word = event.currentTarget.textContent;
+    setSentence((prevSentence) => {
+      const index = prevSentence.findIndex((item) => item.guess === "_");
+      if (index === -1) return prevSentence;
+      return prevSentence.map((item, i) => {
+        if (i === index) {
+          return { word: item.word, guess: `${word?.toLowerCase()}` };
+        } else {
+          return item;
+        }
+      });
+    });
+
+    console.log(sentence);
+  }
+
+  function handleUndo() {
+    setSentence((prevSentence) => {
+      const index = prevSentence.findLastIndex(
+        (item) => item.guess !== "_" && item.guess != null
+      );
+      if (index === -1) return prevSentence;
+      return prevSentence.map((item, i) => {
+        if (i === index) {
+          return { word: item.word, guess: "_" };
+        } else {
+          return item;
+        }
+      });
+    });
+  }
+
+  const displaySentence = sentence
     .map(({ word, guess }) => guess || word)
     .join(" ");
 
@@ -14,9 +47,39 @@ export default function WordGuess() {
       <p>{displaySentence}</p>
       <ul>
         <li>
-          <button>Swim</button>
+          <button onClick={handleGuess}>Swim</button>
+        </li>
+        <li>
+          <button onClick={handleGuess}>Think</button>
+        </li>
+        <li>
+          <button onClick={handleGuess}>Dance</button>
         </li>
       </ul>
+      <button onClick={handleUndo}>Undo</button>
     </>
   );
 }
+
+// setSentence([
+//   {
+//     word: "I",
+//     guess: null,
+//   },
+//   {
+//     word: "think",
+//     guess: `${word?.toLowerCase()}`,
+//   },
+//   {
+//     word: "therefore",
+//     guess: null,
+//   },
+//   {
+//     word: "I",
+//     guess: "_",
+//   },
+//   {
+//     word: "am",
+//     guess: null,
+//   },
+// ]);
